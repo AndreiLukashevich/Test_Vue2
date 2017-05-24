0df92cb53d0a33881bb981ee8bdfc748f7f48f7b
AndreiLukashevich <lukashevichandn@gmail.com>
2017-05-24 06:08:16 +0300

    Initial configuration

---
+++ app-vue2/src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button type="button" class="btn btn-outline-success">Hello world!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
