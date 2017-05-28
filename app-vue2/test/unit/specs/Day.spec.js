ef3d223e9c4855bc1e6c5b2811294b0514f3dc2b
AndreiLukashevich <lukashevichandn@gmail.com>
2017-05-28 23:46:07 +0300

    Added simple tests

--- app-vue2/test/unit/specs/Day.spec.js
+++ app-vue2/test/unit/specs/Day.spec.js
@@ -2,15 +2,7 @@ import Vue from 'vue'
 import Day from '@/components/Day'
 
 describe('Day.vue', () => {
-  it('should return type Object', () => {
-    const Constructor = Vue.extend(Day)
-    const vm = new Constructor().$mount()
-    expect(typeof vm.getInput() === 'object')
-  })
-})
-
-describe('Day.vue', () => {
-  it('Test1', () => {
+  it('Test Data', () => {
     expect(typeof Day.data === 'function')
     const testData = Day.data()
     expect(typeof testData.editSave === 'boolean')
@@ -19,12 +11,46 @@ describe('Day.vue', () => {
   })
 })
 
+// describe('Day.vue', () => {
+//   it('Test Created', () => {
+//     expect(typeof Day.created === 'function')
+//     console.log('testCreated')
+//     const Ctor = Vue.extend(Day)
+//     const vm = new Ctor().$mount()
+//     Day.created()
+//     console.log(typeof id)
+//   })
+// })
+
 describe('Day.vue', () => {
-  it('Test2', () => {
-    expect(typeof Day.created === 'function')
-    console.log('testCreated')
+  it('Test goBack', () => {
     const Ctor = Vue.extend(Day)
     const vm = new Ctor().$mount()
-    console.log(typeof vm.created)
+    expect(typeof vm.goBack === 'function')
+  })
+})
+
+describe('Day.vue', () => {
+  it('Test editTask', () => {
+    const Ctor = Vue.extend(Day)
+    const vm = new Ctor().$mount()
+    expect(typeof vm.editTask === 'function')
+  })
+})
+
+describe('Day.vue', () => {
+  it('Test saveTask', () => {
+    const Ctor = Vue.extend(Day)
+    const vm = new Ctor().$mount()
+    expect(typeof vm.saveTask === 'function')
+  })
+})
+
+describe('Day.vue', () => {
+  it('Test getInput', () => {
+    const Constructor = Vue.extend(Day)
+    const vm = new Constructor().$mount()
+    expect(typeof vm.getInput === 'function')
+    expect(typeof vm.getInput() === 'object')
   })
 })
